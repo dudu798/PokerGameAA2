8bb3056c7421b2d1531bd82bf47b095c14e53b6b
joaomdaltoe <152723667+joaomdaltoe@users.noreply.github.com>
2024-05-30 15:42:32 -0300

    Update baralho.py

--- baralho.py
+++ baralho.py
@@ -1,1 +1,16 @@
 #classe baralho
+from carta import Carta
+class Baralho:
+    def __init__(self):
+        self.cartas = [Carta(naipe, valor) for naipe in Carta.naipes for valor in Carta.valores]
+        random.shuffle(self.cartas)
+        
+    
+    def distribuir(self):
+        return self.cartas.pop()
+    
+    def distribuir_cartas(jogadores, baralho, num_cartas=2):
+        for _ in range(num_cartas):
+            for jogador in jogadores:
+                jogador.receber_carta(baralho.distribuir())
+    
