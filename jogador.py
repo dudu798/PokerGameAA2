473b019b31b8a128de67fffece36da2ba54991d5
joaomdaltoe <152723667+joaomdaltoe@users.noreply.github.com>
2024-05-30 15:43:12 -0300

    Update jogador.py

--- jogador.py
+++ jogador.py
@@ -1,1 +1,17 @@
 # classe jogador
+class Jogador:
+    def __init__(self, nome):
+        self.nome = nome
+        self.mao = []
+
+    def receber_carta(self, carta):
+        self.mao.append(carta)
+
+    def mostrar_mao(self):
+        return ', '.join(map(str, self.mao))
+
+    def distribuir_cartas(jogadores, baralho, num_cartas=2):
+        for _ in range(num_cartas):
+            for jogador in jogadores:
+                jogador.receber_carta(baralho.distribuir())
+
