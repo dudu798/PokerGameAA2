b4339db2fea72bc4b5a5fe3c639d8a8c9e2ccf0b
joaomdaltoe <152723667+joaomdaltoe@users.noreply.github.com>
2024-05-30 15:42:48 -0300

    Update carta.py

--- carta.py
+++ carta.py
@@ -1,1 +1,11 @@
 #classe carta
+class Carta:
+    naipes = ['Copas', 'Ouros', 'Espadas', 'Paus']
+    valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    
+    def __init__(self, naipe, valor):
+        self.naipe = naipe
+        self.valor = valor
+    
+    def __repr__(self):
+        return f"{self.valor} de {self.naipe}"
